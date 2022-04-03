3b176fc60fadd0e3fb401dee2b2d01b8ed38c423
Barbieri-Riccardo <barbieri.riccardo@iisgalvanimi.edu.it>
2022-04-03 18:28:30 +0000

    punto 3

--- email-barbieri/src/app/app.component.ts
+++ email-barbieri/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Email } from 'src/email/mail';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
---
+++ email-barbieri/src/email/mail.ts
@@ -0,0 +1,5 @@
+export class Email{
+    constructor(public a:string,public oggetto:string,public messaggio:string,){
+
+    }
+}
